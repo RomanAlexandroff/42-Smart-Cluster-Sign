b9db45ebc50f00ab19263c553affa184d87e67cf
RomanAlexandroff <r.aleksandroff@gmail.com>
2026-08-22 23:14:39 +0200

    update the logic and fix debugging messages

--- src/display_handling.cpp
+++ src/display_handling.cpp
@@ -6,7 +6,7 @@
 /*   By: raleksan <r.aleksandroff@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 13:00:00 by raleksan          #+#    #+#             */
-/*   Updated: 2024/11/27 18:30:00 by raleksan         ###   ########.fr       */
+/*   Updated: 2026/08/22 18:30:00 by roaleksa         ###   ########.fr       */
 /*                                                                            */
 /*                                                                            */
 /* ************************************************************************** */
@@ -151,13 +151,27 @@ static void draw_bitmap_full_update(const unsigned char* image, uint16_t width, 
 static void draw_exam_screen(EXAM_SCREEN_t screen)
 {
     if (screen == EXAM_SCREEN_PRE_50)
+    {
+        DEBUG_PRINTF("\n[THE DISPLAY] Drawing Pre-exam warning with the \"50 minutes left\" note\n");
         draw_colour_bitmap_raw(preexam_50mins, preexam_warning_red);
+    }
     else if (screen == EXAM_SCREEN_PRE_25)
+    {
+        DEBUG_PRINTF("\n[THE DISPLAY] Drawing Pre-exam warning with the \"25 minutes left\" note\n");
         draw_colour_bitmap_raw(preexam_25mins, preexam_warning_red);
+    }
     else if (screen == EXAM_SCREEN_PRE_5)
+    {
+        DEBUG_PRINTF("\n[THE DISPLAY] Drawing Pre-exam warning with the \"5 minutes left\" note\n");
         draw_colour_bitmap_raw(preexam_5mins, preexam_warning_red);
+    }
     else if (screen == EXAM_SCREEN_ACTIVE)
+    {
+        DEBUG_PRINTF("\n[THE DISPLAY] Drawing full-screen EXAM IN PROGRESS sign\n");
         draw_colour_bitmap_raw(exam_warning_black, exam_warning_red);
+    }
+    else
+        DEBUG_PRINTF("[THE DISPLAY] Unknown EXAM_SCREEN signature! Error!\n");
 }
 
 
@@ -169,28 +183,58 @@ static void draw_exam_screen(EXAM_SCREEN_t screen)
 static void draw_cluster_side(const DISPLAY_STATE_t& state)
 {
     if (state.cluster_side == CLUSTER_SIDE_DEFAULT)
+    {
+        DEBUG_PRINTF("[THE DISPLAY] ...the default cluster icons\n");
         draw_bitmap_partial_update(default_cluster_icons, 170, 480);
+    }
     else if (state.cluster_side == CLUSTER_SIDE_INTRA_ERROR)
+    {
+        DEBUG_PRINTF("[THE DISPLAY] ...the Intra error warning\n");
         draw_bitmap_partial_update(intra_error_img, 170, 480);
+    }
     else if (state.cluster_side == CLUSTER_SIDE_SECRET_EXPIRED)
+    {
+        DEBUG_PRINTF("[THE DISPLAY] ...the Secret expiration warning\n");
         draw_bitmap_partial_update(secret_expire_img, 170, 480);
+    }
     else if (state.cluster_side == CLUSTER_SIDE_EXAM_DAY)
     {
+        DEBUG_PRINTF("[THE DISPLAY] ...the exam time note\n");
         draw_bitmap_partial_update(reserve_note_img, 170, 480);
         draw_exam_start_time(state.exam_hour, state.exam_minutes);
     }
     else if (state.cluster_side == CLUSTER_SIDE_LOW_BATTERY)
+    {
+        DEBUG_PRINTF("[THE DISPLAY] ...the low battery warning\n");
         draw_bitmap_partial_update(low_battery_img, 170, 480);
+    }
     else if (state.cluster_side == CLUSTER_SIDE_OTA_WAITING)
+    {
+        DEBUG_PRINTF("[THE DISPLAY] ...the OTA waiting notification\n");
         draw_text("   WAITING FOR\n   OTA UPDATE", 0, 710);
+    }
     else if (state.cluster_side == CLUSTER_SIDE_OTA_SUCCESS)
+    {
+        DEBUG_PRINTF("[THE DISPLAY] ...the OTA success notification\n");
         draw_text("   OTA UPDATE\n   SUCCESS", 0, 710);
+    }
     else if (state.cluster_side == CLUSTER_SIDE_OTA_FAIL)
+    {
+        DEBUG_PRINTF("[THE DISPLAY] ...the OTA fail notification\n");
         draw_text("   OTA UPDATE\n   FAIL", 0, 710);
+    }
     else if (state.cluster_side == CLUSTER_SIDE_OTA_CANCELED)
+    {
+        DEBUG_PRINTF("[THE DISPLAY] ...the OTA canceled notification\n");
         draw_text("   OTA UPDATE\n   WAS CANCELED", 0, 710);
+    }
     else if (state.cluster_side == CLUSTER_SIDE_TELEGRAM_ERROR)
+    {
+        DEBUG_PRINTF("[THE DISPLAY] ...the Telegram error warning\n");
         draw_text("   TELEGRAM BOT\n   ERROR", 0, 710);
+    }
+    else
+        DEBUG_PRINTF("[THE DISPLAY] ...unknown CLUSTER_SIDE signature! Error!\n");
 }
 
 
@@ -224,28 +268,33 @@ static void filter_request_and_draw(const DISPLAY_STATE_t& requested)
     if (display_cache_valid && same_display_state(display_cache, requested))
     {
         DEBUG_PRINTF("\n[THE DISPLAY] Nothing new to draw. Drawing aborted\n\n");
-        return;
+        return ;
     }
     if (requested.view == DISPLAY_VIEW_BLANK)
     {
+        DEBUG_PRINTF("\n[THE DISPLAY] Drawing a blank display\n\n");
         display.clearScreen();
         display.writeScreenBuffer();
         display_cache = requested;
         display_cache_valid = true;
-        return;
+        return ;
     }
     if (requested.view == DISPLAY_VIEW_EXAM)
     {
         draw_exam_screen(requested.exam_screen);
         display_cache = requested;
         display_cache_valid = true;
-        return;
+        return ;
     }
     if (requested.view == DISPLAY_VIEW_CLUSTER)
     {
         cluster_already_visible = display_cache_valid && (display_cache.view == DISPLAY_VIEW_CLUSTER);
+        DEBUG_PRINTF("\n[THE DISPLAY] Drawing ");
         if (!cluster_already_visible)
+        {
+            DEBUG_PRINTF("the Cluster number with...\n");
             draw_bitmap_full_update(cluster_number_img, 630, 480);
+        }
         if (!cluster_already_visible ||
             display_cache.cluster_side != requested.cluster_side ||
             display_cache.exam_hour != requested.exam_hour ||
@@ -255,114 +304,61 @@ static void filter_request_and_draw(const DISPLAY_STATE_t& requested)
         }
         display_cache = requested;
         display_cache_valid = true;
-        DEBUG_PRINTF("[THE DISPLAY] The drawing process is complete\n");
     }
 }
 
 
-static void display_draw_cluster_default(void)
+/*
+*   Most of the images consists of multiple parts that
+*   have to be printed together in order to achive
+*   the final desirable result. This function contains
+*   "recipes" for all the images that the device can
+*   draw on the display. When it is told what image
+*   the device wants to draw, the function looks up
+*   the recipe for the image and puts it into the state
+*   struct.
+*/
+static bool display_state_init(IMAGE_t image, DISPLAY_STATE_t* state)
 {
-    DISPLAY_STATE_t state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_DEFAULT, EXAM_SCREEN_NONE, 0, 0};
-    DEBUG_PRINTF("[THE DISPLAY] Requested to draw: Cluster number with default cluster icons\n");
-    filter_request_and_draw(state);
-}
-
-static void display_draw_cluster_intra_error(void)
-{
-    DISPLAY_STATE_t state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_INTRA_ERROR, EXAM_SCREEN_NONE, 0, 0};
-    DEBUG_PRINTF("[THE DISPLAY] Requested to draw: Cluster number with Intra error warning\n");
-    filter_request_and_draw(state);
-}
-
-static void display_draw_cluster_secret_expired(void)
-{
-    DISPLAY_STATE_t state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_SECRET_EXPIRED, EXAM_SCREEN_NONE, 0, 0};
-    DEBUG_PRINTF("[THE DISPLAY] Requested to draw: Cluster number with Secret expiration warning\n");
-    filter_request_and_draw(state);
-}
-
-static void display_draw_cluster_low_battery(void)
-{
-    DISPLAY_STATE_t state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_LOW_BATTERY, EXAM_SCREEN_NONE, 0, 0};
-    DEBUG_PRINTF("[THE DISPLAY] Requested to draw: Cluster number with low battery warning\n");
-    filter_request_and_draw(state);
-}
-
-static void display_draw_cluster_ota_waiting(void)
-{
-    DISPLAY_STATE_t state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_OTA_WAITING, EXAM_SCREEN_NONE, 0, 0};
-    DEBUG_PRINTF("[THE DISPLAY] Requested to draw: Cluster number with OTA waiting notification\n");
-    filter_request_and_draw(state);
-}
-
-static void display_draw_cluster_ota_success(void)
-{
-    DISPLAY_STATE_t state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_OTA_SUCCESS, EXAM_SCREEN_NONE, 0, 0};
-    DEBUG_PRINTF("[THE DISPLAY] Requested to draw: Cluster number with OTA success notification\n");
-    filter_request_and_draw(state);
-}
-
-static void display_draw_cluster_ota_fail(void)
-{
-    DISPLAY_STATE_t state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_OTA_FAIL, EXAM_SCREEN_NONE, 0, 0};
-    DEBUG_PRINTF("[THE DISPLAY] Requested to draw: Cluster number with failed OTA update notification\n");
-    filter_request_and_draw(state);
-}
-
-static void display_draw_cluster_ota_canceled(void)
-{
-    DISPLAY_STATE_t state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_OTA_CANCELED, EXAM_SCREEN_NONE, 0, 0};
-    DEBUG_PRINTF("[THE DISPLAY] Requested to draw: Cluster number with canceled OTA update notification\n");
-    filter_request_and_draw(state);
-}
-
-static void display_draw_cluster_telegram_error(void)
-{
-    DISPLAY_STATE_t state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_TELEGRAM_ERROR, EXAM_SCREEN_NONE, 0, 0};
-    DEBUG_PRINTF("[THE DISPLAY] Requested to draw: Cluster number with Telegram error warning\n");
-    filter_request_and_draw(state);
-}
-
-static void display_draw_cluster_exam_day(uint16_t hour, uint16_t minutes)
-{
-    DISPLAY_STATE_t state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_EXAM_DAY, EXAM_SCREEN_NONE, hour, minutes};
-    DEBUG_PRINTF("[THE DISPLAY] Requested to draw: Cluster number with exam time notification\n");
-    filter_request_and_draw(state);
-}
-
-static void display_draw_exam_pre_50(void)
-{
-    DISPLAY_STATE_t state = {DISPLAY_VIEW_EXAM, CLUSTER_SIDE_NONE, EXAM_SCREEN_PRE_50, 0, 0};
-    DEBUG_PRINTF("[THE DISPLAY] Requested to draw: Full-screen pre-exam warning with 50 mins left\n");
-    filter_request_and_draw(state);
-}
-
-static void display_draw_exam_pre_25(void)
-{
-    DISPLAY_STATE_t state = {DISPLAY_VIEW_EXAM, CLUSTER_SIDE_NONE, EXAM_SCREEN_PRE_25, 0, 0};
-    DEBUG_PRINTF("[THE DISPLAY] Requested to draw: Full-screen pre-exam warning with 25 mins left\n");
-    filter_request_and_draw(state);
-}
-
-static void display_draw_exam_pre_5(void)
-{
-    DISPLAY_STATE_t state = {DISPLAY_VIEW_EXAM, CLUSTER_SIDE_NONE, EXAM_SCREEN_PRE_5, 0, 0};
-    DEBUG_PRINTF("[THE DISPLAY] Requested to draw: Full-screen pre-exam warning with 5 mins left\n");
-    filter_request_and_draw(state);
-}
-
-static void display_draw_exam_active(void)
-{
-    DISPLAY_STATE_t state = {DISPLAY_VIEW_EXAM, CLUSTER_SIDE_NONE, EXAM_SCREEN_ACTIVE, 0, 0};
-    DEBUG_PRINTF("[THE DISPLAY] Requested to draw: Full-screen Exam sign\n");
-    filter_request_and_draw(state);
-}
-
-static void display_clear(void)
-{
-    DISPLAY_STATE_t state = {DISPLAY_VIEW_BLANK, CLUSTER_SIDE_NONE, EXAM_SCREEN_NONE, 0, 0};
-    DEBUG_PRINTF("[THE DISPLAY] Requested to clear the display\n");
-    filter_request_and_draw(state);
+    if (!state)
+        return (false);
+    *state = {DISPLAY_VIEW_INVALID, CLUSTER_SIDE_NONE, EXAM_SCREEN_NONE, 0, 0};
+    if (image == CLUSTER_DEFAULT)
+        *state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_DEFAULT, EXAM_SCREEN_NONE, 0, 0};
+    else if (image == INTRA_ERROR)
+        *state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_INTRA_ERROR, EXAM_SCREEN_NONE, 0, 0};
+    else if (image == SECRET_EXPIRED)
+        *state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_SECRET_EXPIRED, EXAM_SCREEN_NONE, 0, 0};
+    else if (image == EXAM_DAY)
+        *state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_EXAM_DAY, EXAM_SCREEN_NONE, rtc_g.exam_start_hour, rtc_g.exam_start_minutes};
+    else if (image == LOW_BATTERY)
+        *state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_LOW_BATTERY, EXAM_SCREEN_NONE, 0, 0};
+    else if (image == OTA_WAITING)
+        *state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_OTA_WAITING, EXAM_SCREEN_NONE, 0, 0};
+    else if (image == OTA_SUCCESS)
+        *state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_OTA_SUCCESS, EXAM_SCREEN_NONE, 0, 0};
+    else if (image == OTA_FAIL)
+        *state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_OTA_FAIL, EXAM_SCREEN_NONE, 0, 0};
+    else if (image == OTA_CANCELED)
+        *state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_OTA_CANCELED, EXAM_SCREEN_NONE, 0, 0};
+    else if (image == TELEGRAM_ERROR)
+        *state = {DISPLAY_VIEW_CLUSTER, CLUSTER_SIDE_TELEGRAM_ERROR, EXAM_SCREEN_NONE, 0, 0};
+    else if (image == PREEXAM_50)
+        *state = {DISPLAY_VIEW_EXAM, CLUSTER_SIDE_NONE, EXAM_SCREEN_PRE_50, 0, 0};
+    else if (image == PREEXAM_25)
+        *state = {DISPLAY_VIEW_EXAM, CLUSTER_SIDE_NONE, EXAM_SCREEN_PRE_25, 0, 0};
+    else if (image == PREEXAM_5)
+        *state = {DISPLAY_VIEW_EXAM, CLUSTER_SIDE_NONE, EXAM_SCREEN_PRE_5, 0, 0};
+    else if (image == EXAM_ACTIVE)
+        *state = {DISPLAY_VIEW_EXAM, CLUSTER_SIDE_NONE, EXAM_SCREEN_ACTIVE, 0, 0};
+    else if (image == BLANK)
+        *state = {DISPLAY_VIEW_BLANK, CLUSTER_SIDE_NONE, EXAM_SCREEN_NONE, 0, 0};
+    else
+    {
+        DEBUG_PRINTF("[THE DISPLAY] Unsupported image requested: %d\n", image);
+        return (false);
+    }
+    return (true);
 }
 
 
@@ -372,42 +368,12 @@ static void display_clear(void)
 */
 void    draw_on_display(IMAGE_t image)
 {
-    if (image == EXAM_ACTIVE)
-        display_draw_exam_active();
-    else if (image == PREEXAM_5)
-        display_draw_exam_pre_5();
-    else if (image == PREEXAM_25)
-        display_draw_exam_pre_25();
-    else if (image == PREEXAM_50)
-        display_draw_exam_pre_50();
-    else if (image == CLUSTER_DEFAULT)
-        display_draw_cluster_default();
-    else if (image == INTRA_ERROR)
-        display_draw_cluster_intra_error();
-    else if (image == SECRET_EXPIRED)
-        display_draw_cluster_secret_expired();
-    else if (image == EXAM_DAY)
-        display_draw_cluster_exam_day(rtc_g.exam_start_hour, rtc_g.exam_start_minutes);
-    else if (image == LOW_BATTERY)
-        display_draw_cluster_low_battery();
-    else if (image == OTA_WAITING)
-        display_draw_cluster_ota_waiting();
-    else if (image == OTA_SUCCESS)
-        display_draw_cluster_ota_success();
-    else if (image == OTA_FAIL)
-        display_draw_cluster_ota_fail();
-    else if (image == OTA_CANCELED)
-        display_draw_cluster_ota_canceled();
-    else if (image == TELEGRAM_ERROR)
-        display_draw_cluster_telegram_error();
-    else if (image == BLANK)
-        display_clear();
-    else
-    {
-        DEBUG_PRINTF("[THE DISPLAY] Unsupported image requested: %d\n", image);
-        return;
-    }
-    DEBUG_PRINTF("[THE DISPLAY] The drawing process is complete\n");
+    DISPLAY_STATE_t state;
+
+    if (!display_state_init(image, &state))
+        return ;
+    filter_request_and_draw(state);
+    DEBUG_PRINTF("[THE DISPLAY] The drawing process is concluded\n");
 }
 
 
